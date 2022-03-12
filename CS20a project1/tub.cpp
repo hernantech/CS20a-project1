a4d2e9312d84ed5bffdfdbee30d7002aab4ef564
Alex <ahernan2@caltech.edu>
2022-03-11 17:05:35 -0800

    uhhghgh

---
+++ CS20a project1/tub.cpp
@@ -0,0 +1,8 @@
+//
+//  tub.cpp
+//  CS20a project1
+//
+//  Created by OOOH YEAH on 3/11/22.
+//
+
+#include "tub.hpp"
---
+++ CS20a project1/tub.hpp
@@ -0,0 +1,13 @@
+//
+//  tub.hpp
+//  CS20a project1
+//
+//  Created by on 3/11/22.
+//
+
+#ifndef tub_hpp
+#define tub_hpp
+
+#include <stdio.h>
+
+#endif /* tub_hpp */
